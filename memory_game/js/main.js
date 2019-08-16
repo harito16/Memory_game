4f13b49c67faaa9da5c5fee4275c8a23f0742c59
Alejandro Haro <harito16@me.com>
2019-08-16 17:15:51 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,18 +2,33 @@ console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[3];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push('cardTwo');
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardTwo);
 
- if (cardsInPlay.length === 2) {
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-      alert('You found a match');
-    } else {
-      alert('Sorry, try again');
-    }
+
+  function checkForMatch() {
+  	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
 }
+
+}
+
+function flipCard(cardID) {
+	console.log("User flipped " + cards[cardID]);
+    cardsInPlay.push(cards[cardID])
+
+ checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+
+	if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[2]) {
+    	alert('You found a math');
+    } else {
+    	alert ('Sorry, try again');
+      
+ }
+    };
